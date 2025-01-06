41aec71a26a64fa490d72cb6a4b07a82ee32720d
Saber <11201752-sabersservant@users.noreply.replit.com>
2025-01-06 04:08:20 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@C-te/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> brainrot = {"Sigma", "Alpha", "Goon", "Rizz"};
+
+    for (string term : brainrot) {
+        cout << term << "\n";
+    }
+
+    return 0;
+}
